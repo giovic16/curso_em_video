ac21cc39ff6b7478a10f9c4aaa538e386879ee75
Giovana Victória <giovanavictoria887@gmail.com>
2021-07-30 17:37:36 -0300

    mundoum

---
+++ ex004.py
@@ -0,0 +1,10 @@
+# Faça um programa que leia algo pelo teclado e mostre
+# seu tipo primitivo e todas as informações possíveis sobre ele
+
+algo = input('Digite um algo:')
+print('A entrada é um número?', algo.isnumeric())
+print('A entrada é uma letra?', algo.isalpha())
+print('A entrada é alfanumérica (letras e números)?', algo.isalnum())
+print('A entrada possui somente letras maiúsculas?', algo.isupper())
+print('A entrada possui somente letras minúsculas?', algo.islower())
+print('A entrada possui espaços', algo.isspace())
